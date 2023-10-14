8c4b8fb51598591a697734110895a17e5de46917
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-14 22:40:04 +0200

    Header component + authContext added and statments

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import classes from './Header.module.css'
+import AuthContext from '../context/AuthContext'
+
+const Header = () => {
+    const { user } = useContext(AuthContext)
+    return (
+        <header>
+            <div className={classes.header}>
+                <Link className={classes['header-title']}>AIRPORT</Link>
+                <nav className={classes['header-menu']}>
+                    {user ? (
+                        <Link className={classes['header-menu-link-auth']}>Logout</Link>
+                    ) : (
+                        <>
+                            <Link className={classes['header-menu-link-auth']} to='/login'>Login</Link>
+                            <Link className={classes['header-menu-link-auth']} to='/register'>Sign Up</Link>
+                        </>
+                    )}
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
