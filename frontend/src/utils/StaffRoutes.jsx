1fe4b4eafb19a022f406685d374f8fc63f8c7b39
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-14 22:40:56 +0200

    Private Routes + StaffRoutes template for future

---
+++ frontend/src/utils/StaffRoutes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const StaffRoutes = () => {
+    return (
+        <div>StaffRoutes</div>
+    )
+}
+
+export default StaffRoutes
